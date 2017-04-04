3c0c28f8a89c7f9df51b04ce858a649003d5027f
Max Lipp <mx5@mailbox.org>
2017-04-04 23:26:36 +0200

    ..

--- src/main/java/edu/hm/cs/apfel/reflection/App.java
+++ src/main/java/edu/hm/cs/apfel/reflection/App.java
@@ -1,14 +1,28 @@
 package edu.hm.cs.apfel.reflection;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+
+    public static void main(String... args) throws ReflectiveOperationException {
+        Class<?> type = String.class;
+
+        Constructor<?> ctor = type.getConstructor(char[].class);
+        Object s = ctor.newInstance(new char[] {'a'});
+        System.out.println((String)s);
+        System.out.println(x);
+
+
+    }
     @RenderMe(with = "blah")
     private int test = 4;
 
     @RenderMe
     public String test2 = "asdf";
 }
+
