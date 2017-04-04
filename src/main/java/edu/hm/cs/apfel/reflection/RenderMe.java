bc679b3c67823f5c6ce59752ad3887589c21526a
mountainflo <tobusch@hm.edu>
2017-04-04 10:04:57 +0200

    annotation with retention and target

---
+++ src/main/java/edu/hm/cs/apfel/reflection/RenderMe.java
@@ -0,0 +1,28 @@
+/**
+ * Organisation: Hochschule Muenchen, Fakultaet 07 Informatik und Mathematik
+ * Purpose: lab software-architecture, IF4B, SS2017
+ * Purpose: solution of assignment 1
+ */
+
+package edu.hm.cs.apfel.reflection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Maximilian Lipp, lipp@hm.edu
+ * @author Florian Tobusch, tobusch@hm.edu
+ * @version 01.04.2017
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface RenderMe {
+    /**
+     * For own render-Classes which implement a render-method.
+     * @return all fields with their values.
+     */
+    String with() default "";
+
+}
