0e121d76184a46ba67c929989a8678da3249f75b
mountainflo <tobusch@hm.edu>
2017-04-05 11:09:28 +0200

    add class

---
+++ src/main/java/edu/hm/cs/apfel/reflection/ArrayRenderer.java
@@ -0,0 +1,15 @@
+/**
+ * Organisation: Hochschule Muenchen, Fakultaet 07 Informatik und Mathematik
+ * Purpose: lab software-architecture, IF4B, SS2017
+ * Purpose: solution of assignment 1
+ */
+
+package edu.hm.cs.apfel.reflection;
+
+/**
+ * @author Maximilian Lipp, lipp@hm.edu
+ * @author Florian Tobusch, tobusch@hm.edu
+ * @version 01.04.2017
+ */
+public class ArrayRenderer {
+}
