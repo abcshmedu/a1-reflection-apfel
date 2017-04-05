372b95c6c42557f528394a57b7b30802a823d48c
mountainflo <tobusch@hm.edu>
2017-04-05 16:35:50 +0200

    delete system.out.println // add test

--- src/test/java/edu/hm/cs/apfel/reflection/RendererTest.java
+++ src/test/java/edu/hm/cs/apfel/reflection/RendererTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * @author Maximilian Lipp, lipp@hm.edu
@@ -27,7 +28,8 @@ public class RendererTest {
                     { "ObjectWithPrivateString", new Renderer( new Object() {@RenderMe private String publicStringField = "this is a test-String";} ), "Instance of null:\npublicStringField(Type String): this is a test-String\n" },
                     { "ObjectWithPrivateStringWithoutAnnotation", new Renderer( new Object() {private String publicStringField = "this is a test-String";} ), "Instance of null:\n" },
                     { "ObjectWithPrivateBoolean", new Renderer( new Object() {@RenderMe private boolean privateBooleanField = false;} ), "Instance of null:\nprivateBooleanField(Type boolean): false\n" },
-                    { "ObjectWithArrayRenderer", new Renderer( new Object() {@RenderMe(with = "edu.hm.cs.apfel.reflection.ArrayRenderer") private int[] privateIntArray = new int[]{1,2,3};} ), "Instance of null:\nprivateIntArray(Type int[]) [1, 2, 3, ]\n" }
+                    { "ObjectWithArrayRenderer", new Renderer( new Object() {@RenderMe(with = "edu.hm.cs.apfel.reflection.ArrayRenderer") private int[] privateIntArray = new int[]{1,2,3};} ), "Instance of null:\nprivateIntArray(Type int[]) [1, 2, 3, ]\n" },
+                    { "ObjectWithDate", new Renderer( new Object() {@RenderMe private Date privateDateField = new Date(123456789);} ), "Instance of null:\nprivateDateField(Type Date): Fri Jan 02 11:17:36 CET 1970\n" }
             });
         }
 
